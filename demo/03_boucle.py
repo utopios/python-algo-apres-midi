c1b193a7000cc982a9da87de9377a720722ca3d4
ihab abadi <ihab@utopios.net>
2024-03-13 14:38:33 +0100

    utilisation des boucles

---
+++ demo/03_boucle.py
@@ -0,0 +1,25 @@
+# increment = 1
+# while increment < 20:
+#     print("Je me répète !!!!")
+#     increment += 3
+
+# for index in range(100, 21,-3):
+#     print(f"Je me repete Numéro {index} !!!!")
+
+# for index in range(1,10):
+#     print(f"Je me repete Numéro {index} !!!!")
+#     if index > 5 and index%2==0:
+#         break
+
+# for i in range(1, 10):
+#     print(f"i : {i}")
+#     for j in range(1, 10):
+#         print(f"J : {j}")
+#         if j >=4:
+#             break
+
+for i in range(1, 10):
+    if i%2 == 0:
+        print(f"I : {i}")
+    else:
+        continue
